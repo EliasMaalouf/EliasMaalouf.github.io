f1145a18b65e194977706bef5aaf0a181dbe3ca5
unknown <emaal080@uottawa.ca>
2021-07-14 13:36:34 -0400

    done styling and fully functional

--- Lab9/React/myapp/src/components/Body.js
+++ Lab9/React/myapp/src/components/Body.js
@@ -1,41 +1,34 @@
 import React, { Fragment } from 'react';
 import aboutus from "./images/information.png";
-import join from "./images/soccer.png";
-import school from "./images/school.png";
-import trainers from "./images/trainers.png";
-import calendar from "./images/calendar.png";
-import goalkeeper from "./images/casillas.PNG"
-import defender from "./images/kompany.PNG"
-import midfield from "./images/guardiola.PNG"
-import attacker from "./images/shev.PNG"
+import nut from "./images/nuts.jpg";
+import menu from "./images/menu.png";
+import pistach from "./images/pistachios.jpg";
+import cashew from "./images/cashew.jpg";
+import almond from "./images/almond.jpg";
+import peanuts from "./images/peanuts.jpg";
 import help from "./images/help.png";
-
-import Accordion from 'react-bootstrap/Accordion';
-import Card from "react-bootstrap/Card"
+import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { ListGroup } from 'react-bootstrap'; 
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { useEffect, useState } from "react";
-import CardDeck from 'react-bootstrap/CardDeck'
-
-
-
+import CardDeck from 'react-bootstrap/CardDeck';
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import { ModalBody } from 'react-bootstrap';
+
 
 
 function Body(){
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-    return(       
+    return(  
+          
     <Container>
         <Jumbotron fluid>
             <Container>
-                <h1>Go Nuts</h1>
+                <h1 > <img src={nut} id = "img1" alt="" className="icon"/> Go Nuts</h1>
             </Container>
            
         </Jumbotron>
@@ -50,7 +43,7 @@ function Body(){
                 <br></br>
 
             </Container>
-            <Button>Browse our selection</Button>
+            <a href = "#menu" className="link">Browse our selection{">"}</a>
         </Jumbotron>
 
 
@@ -70,208 +63,233 @@ function Body(){
            
         </Jumbotron>
 
-        <div class ="p-3 my-3 bg">
-    <h1 class=""> <img src={trainers} alt="" className="icon"/> Our Menu</h1><br></br>
+        <div class ="p-3 my-3 bg" id = "menu">
+        <Jumbotron fluid>
+            <Container>
+            <h1 class="" > <img src={menu} alt="" className="icon1"/> Our Menu</h1><br></br>
 
-    <CardDeck>
-  <Card>
-    <Card.Img variant="top" src={goalkeeper}/>
-    <Card.Body>
-      <Card.Title>Piss taschois</Card.Title>
-      <Card.Text>
-      Iker Casillas is an experienced goalkeeper coach who has helped several young goalkeepers go pro.
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src={defender}/>
-    <Card.Body>
-      <Card.Title>Vincent Kompany</Card.Title>
-      <Card.Text>
-      Vincent Kompany is an experienced defender coach who has helped several young defenders go pro.
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src={midfield}/>
-    <Card.Body>
-      <Card.Title>Pep Guardiola</Card.Title>
-      <Card.Text>
-      Pep Guardiola is an experienced midfielder coach who has helped several young midfielders go pro.
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src={attacker}/>
-    <Card.Body>
-      <Card.Title>Andriy Shevchenko</Card.Title>
-      <Card.Text>
-      Andriy Shevchenko is an experience forward coach who has helped several young forwards go pro.
-      </Card.Text>
-    </Card.Body>
-  </Card>
+<CardDeck>
+<Card>
+<Card.Img variant="top" src={pistach}/>
+<Card.Body>
+<br></br>
+  <Card.Title>Pistachios</Card.Title>
+  
+  <Card.Text>
+  The Pistachio is a memeber of the cashew family, is a small tree originating from Central Asia and the Middle East.
+  </Card.Text>
+  
+</Card.Body>
+<Card.Footer>
+<Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
+  <Button variant="primary" onClick={handleShow}>
+    Order Now!
+  </Button>
+  <Modal id="md1" show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Order Now!</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Group controlId="name">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="name" placeholder="Full Name" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Home address</Form.Label>
+          <Form.Control type="adress" placeholder="123 test dr" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={handleClose}>
+        Save Changes
+      </Button>
+    </Modal.Footer>
+  </Modal>
+</Card.Footer>
+</Card>
+<Card>
+<Card.Img variant="top" src={cashew}/>
+<Card.Body>
+  <Card.Title>Cashews</Card.Title>
+  <Card.Text>
+  The cashew tree (Anacardium occidentale) is a tropical evergreen tree that produces the cashew seed and the cashew apple pseudofruit.
+  </Card.Text>
+  
+</Card.Body>
+<Card.Footer>
+<Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
+  <Button variant="primary" onClick={handleShow}>
+    Order Now!
+  </Button>
+  <Modal id="md1" show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Order Now!</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Group controlId="name">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="name" placeholder="Full Name" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Home address</Form.Label>
+          <Form.Control type="adress" placeholder="123 test dr" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={handleClose}>
+        Save Changes
+      </Button>
+    </Modal.Footer>
+  </Modal>
+</Card.Footer>
+</Card>
+<Card>
+<Card.Img variant="top" src={almond}/>
+<Card.Body>
+<br></br>
+<br></br>
+  <Card.Title>Almonds</Card.Title>
+  <Card.Text>
+  Almonds are the edible seeds of Prunus dulcis, more commonly called the almond tree.
+  </Card.Text>
+  
+</Card.Body>
+<Card.Footer>
+<Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
+  <Button variant="primary" onClick={handleShow}>
+    Order Now!
+  </Button>
+  <Modal id="md1" show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Order Now!</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Group controlId="name">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="name" placeholder="Full Name" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Home address</Form.Label>
+          <Form.Control type="adress" placeholder="123 test dr" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={handleClose}>
+        Save Changes
+      </Button>
+    </Modal.Footer>
+  </Modal>
+</Card.Footer>
+</Card>
+<Card>
+<Card.Img variant="top" src={peanuts}/>
+<Card.Body>
+  <Card.Title>Peanuts</Card.Title>
+  <Card.Text>
+  The peanut, also known as the groundnut,[2] goober (US),[3] pindar (US)[3] or monkey nut (UK), and taxonomically classified as Arachis hypogaea, is a legume crop grown mainly for its edible seeds.
+  </Card.Text>
+</Card.Body>
+<Card.Footer>
+<Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
+  <Button variant="primary" onClick={handleShow}>
+    Order Now!
+  </Button>
+  <Modal id="md1" show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Please fill out the required information</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Group controlId="name">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="name" placeholder="Full Name" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="adress">
+          <Form.Label>Home address</Form.Label>
+          <Form.Control type="adress" placeholder="123 test dr" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+        <Form.Group controlId="credit">
+          <Form.Label>Credit Card#</Form.Label>
+          <Form.Control type="credit" placeholder="XXXX-XXXX-XXXX-XXXX" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
+        </Form.Group>
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={handleClose}>
+        Place Order
+      </Button>
+    </Modal.Footer>
+  </Modal>
+</Card.Footer>
+</Card>
 </CardDeck>
+            </Container>
+           
+        </Jumbotron>
+    
 </div>    
 
-    
-
-
+<div class ="p-3 my-3 bg">
+<Jumbotron fluid>
+            <Container>
+            <h1 class=""> <img src={help} alt="" className="icon"/> Help</h1><br></br>
+    <h5>Frequently Asked Questions</h5>
+    <ListGroup>
+    <ListGroup.Item><b>Q: Where is Go Nuts located?</b> <br></br>
+                    A: We are located on 4529 HuntClub rd.
+    </ListGroup.Item>
+    <ListGroup.Item><b>Q: Are the nuts freshly roasted everyday?</b> <br></br>
+                    A: YES! We have a new batch of nuts roasted every morning.
+    </ListGroup.Item>
+    </ListGroup>
+    <br></br>
+    <h5>Please contact us for any other concerns that have not been answered in the frequently asked questions</h5>
+    <ListGroup>
+    <ListGroup.Item><b>Phone number</b> <br></br>
+    613-258-4155 <br></br>Mon - Sat, 8:00am-5:00pm    </ListGroup.Item>
+    <ListGroup.Item><b>Email Address</b> <br></br>
+    nuts@gmail.com
+    </ListGroup.Item>
+    </ListGroup>
+    <br></br>
+            </Container>
+           
+        </Jumbotron>
   
-
-    <div class ="p-3 my-3 bg">
-    <h1 class=""> <img src={join} alt="" className="icon"/> Join the Academy</h1><br></br>
-    <Form>
-<Form.Group controlId="name">
-    <Form.Label>Full Name</Form.Label>
-    <Form.Control type="name" placeholder="Full Name" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
-  </Form.Group>
-  <Form.Group controlId="email">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" onchange="if (typeof this.reportValidity === 'function') {this.reportValidity();}"/>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlSelect1">
-    <Form.Label>Age</Form.Label>
-    <Form.Control as="select">
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
-      <option>10</option>
-      <option>11</option>
-      <option>12</option>
-      <option>13</option>
-      <option>14</option>
-      <option>15</option>
-      <option>16</option>
-      <option>17</option>
-      <option>18</option>
-      <option>19</option>
-      <option>20</option>
-    </Form.Control>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlSelect2">
-    <Form.Label>Soccer position</Form.Label>
-    <Form.Control as="select" multiple>
-      <option>Goalkeeper</option>
-      <option>Defender</option>
-      <option>Midfielder</option>
-      <option>Forward</option>
-    </Form.Control>
-  </Form.Group>
-  
-  <Form.Group controlId="exampleForm.ControlSelect3">
-    <Form.Label>Years of experience</Form.Label>
-    <Form.Control as="select" multiple>
-      <option>0-1</option>
-      <option>2-3</option>
-      <option>3-4</option>
-      <option>5+</option>
-    </Form.Control>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Example textarea</Form.Label>
-    <Form.Control as="textarea" rows={3} />
-  </Form.Group>
-</Form>
-</div>
-
-<div class ="p-3 my-3 bg">
-    <h1 class=""> <img src={school} alt="" className="icon"/> Courses</h1><br></br>
-    <>
-    <Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
-      <Button variant="primary" onClick={handleShow}>
-        GoalKeeper training
-      </Button>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>GoalKeeper training</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-
-    <><Fragment>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
-      <Button variant="primary" onClick={handleShow}>
-        Defense training
-      </Button>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Defense training</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Defense</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-    <><Fragment>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
-      <Button variant="primary" onClick={handleShow}>
-        Midfield training
-      </Button>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Midfield training</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Midfield</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-    <><Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
-      <Button variant="primary" onClick={handleShow}>
-        Forward training
-      </Button>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Forward training</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Forward</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-
-</div>    
-
-
-
-<div class ="p-3 my-3 bg">
-    <h1 class=""> <img src={calendar} alt="" className="icon"/> Book a Training Session</h1><br></br>
-    
-
-
-</div>
-
-
-<div class ="p-3 my-3 bg">
-    <h1 class=""> <img src={help} alt="" className="icon"/> Help</h1><br></br>
-    
-
-
 </div>        
 </Container>
     );
